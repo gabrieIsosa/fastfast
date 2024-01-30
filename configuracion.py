0577af32ba230bc66950841e94867acf178623c3
Gabriel Sosa <118233752+gabrieIsosa@users.noreply.github.com>
2024-01-30 18:08:44 -0300

    Add files via upload

---
+++ configuracion.py
@@ -0,0 +1,22 @@
+from collections import namedtuple
+
+TAMANNO_LETRA = 30
+TAMANNO_LETRA_MED = 50
+FPS_inicial = 3
+TIEMPO_MAX = 61
+
+ANCHO = 800
+ALTO = 600
+COLOR_LETRAS = (20,200,20)
+COLOR_FONDO = (0,0,0)
+COLOR_TEXTO = (200,200,200)
+COLOR_TIEMPO_FINAL = (200,20,10)
+ESPACIO = 50
+Punto = namedtuple('Punto','x y')
+
+#Parametros agregados
+#DIFICULTAD=2
+#CANTIDAD_PAISES=5   #cantidad maxima de paises en pantalla
+
+
+
